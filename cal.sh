b2d6c81317636f39b871891e7f78916fee11535d
kimdaye0000 <daye0529@cu.ac.kr>
2022-04-03 14:45:41 +0900

    add daye2

--- cal.sh
+++ cal.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-a=$(<num1.txt)
-b=$(<num2.txt)
+#a=$(<num1.txt)
+#b=$(<num2.txt)
+
+arr=(`cat num1.txt` `cat num2.txt`)
 
 PS3='select menu: '
 
@@ -10,23 +12,23 @@ do
 	break
 done
 
-echo "num1:$a"
-echo "num2:$b"
+echo "num1:${arr[0]}"
+echo "num2:${arr[1]}"
 echo "op:$cal"
 
 if [ ${cal} == "add" ]
 then
-	add=`expr $a + $b`
+	add=`expr ${arr[0]} + ${arr[1]}`
 	echo "result:$add"
 elif [ ${cal} == "sub" ]
 then
-	sub=`expr $a - $b`
+	sub=`expr ${arr[0]} - ${arr[1]}`
 	echo "result:$sub"
 elif [ ${cal} == "div" ]
 then
-	div=`expr $a / $b`
+	div=`expr ${arr[0]} / ${arr[1]}`
 	echo "result:$div"
 else
-	mul=`expr $a \* $b`
+	mul=`expr ${arr[0]} \* ${arr[1]}`
 	echo "result:$mul"
 fi 
