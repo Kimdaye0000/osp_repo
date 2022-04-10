7be1cffc0b5b1031338282b27419d58294031838
kimdaye0000 <daye0529@cu.ac.kr>
2022-04-11 03:06:26 +0900

    Hw05-2

--- word_freq.py
+++ word_freq.py
@@ -2,14 +2,15 @@ import sys
 
 a= sys.argv[1]
 b= sys.argv[2]
-print(sys.argv[0])
-print(a)
-print(int(b))
+#print(sys.argv[0])
+#print(a)
+#print(int(b))
 
 file = open(a,'r')
 #content = '\nmy name is abc!'
 content = file.read()
 #file.write(content)
 #lines = file.readlines()
-print(content)
+content1=content.split()
+print(content1)
 file.close() 
