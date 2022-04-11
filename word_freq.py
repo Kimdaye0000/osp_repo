31eea389350fe52c950f6c570f490fe5a4909bb5
kimdaye0000 <daye0529@cu.ac.kr>
2022-04-11 23:05:21 +0900

    0411-4

--- word_freq.py
+++ word_freq.py
@@ -18,16 +18,8 @@ for name in content4:
 		counts[name] = 1
 	else:
 		counts[name] += 1
-
-#for key in counts:
-#	print(key, counts[key])
-
 pgm_lang_val_reverse = sorted(counts.items(),reverse=True, key=lambda item: item[1]) 
 
-for key, value in pgm_lang_val_reverse:
-	print(key,  value)
-
-
-
-
+for key, value in pgm_lang_val_reverse[:3]:
+	print(key, value)
 file.close() 
